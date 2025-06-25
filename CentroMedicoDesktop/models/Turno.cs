967de0f74b5f2ba8ff0b0e1505c9d1423d37a05a
AgustinPereezBarrionuevo <agustin_0100@hotmail.com>
2025-06-25 18:55:07 -0300

    Actualizacion para base de datos

---
+++ CentroMedicoDesktop/models/Turno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentroMedicoDesktop.models
+{
+    internal class Turno
+    {
+        public int Id { get; set; }
+        public string PacienteId { get; set; }
+        public int ProfesionalId { get; set; }
+
+        public DateTime FechaHora { get; set; }
+        public string Estado { get; set; }
+    }
+}
